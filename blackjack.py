1f07e652898d36275852c3dc60a4de63782fb193
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2024-02-18 13:46:11 +1300

    added hit and stand functions to player class

--- blackjack.py
+++ blackjack.py
@@ -1,19 +1,38 @@
-import random
+from random import choice
+
 class Casino:
-    @staticmethod
-    def formDeck(aceCardValue:int) -> tuple:
-        return [i for i in (list(range(2,11))+[10,10,10,aceCardValue]) for _ in range(4)]
+    deck = [i for i in (list(range(2,11))+[10,10,10,11]) for _ in range(4)]
+
 class Player:
+
     def __init__(self,name:str) -> None:
         self.__name = name
         self.__deck = []
         self.__balance = 50
+
     @property
     def Name(self) -> str:
         return self.__name
+    
     @property
     def Balance(self) -> int:
         return self.__balance
+    
     @Balance.setter
     def Balance(self,inputBal:int):
         self.__balance = inputBal
+
+    def hit(self) -> tuple:
+        self.__deck.append(Casino.deck.pop(choice(Casino.deck)))
+        return (self.__deck[-1],sum(self.__deck))
+    
+    def stand(self) -> int:
+        return sum(self.__deck)
+    
+def playRound(player:Player,dealer:Player):
+
+player = Player(input("Input Username: "))
+dealer = Player("Dealer")
+
+while player.Balance > 0:
+    playRound(player,dealer)
